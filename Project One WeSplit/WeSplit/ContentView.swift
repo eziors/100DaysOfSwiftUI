0f9b6a67355102a050e2f4773e91e76826041398
eziors <marcos.barbosa@mail.uft.edu.br>
2023-10-18 17:37:25 -0300

    Formatting with swift format

--- Project One WeSplit/WeSplit/ContentView.swift
+++ Project One WeSplit/WeSplit/ContentView.swift
@@ -1,60 +1,54 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-    
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
     @State private var tipPercentage: Int = 0
     @FocusState private var amountIsFocused: Bool
 
-    
-    let tipPercentages: Array<Int> = [0,10,15,20,25]
-    
+    let tipPercentages: [Int] = [0, 10, 15, 20, 25]
+
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-        
+
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
 
         return amountPerPerson
-        
     }
-    
+
     var body: some View {
-        
-        NavigationStack{
+        NavigationStack {
             Form {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
-                    
+
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<21){
+                        ForEach(2 ..< 21) {
                             Text("\($0) people")
                         }
                     }
                 }
-                
-                Section("How much tip do you want to leave?"){
-                    
-                    Picker("Tip percentage", selection: $tipPercentage){
-                        ForEach(tipPercentages, id: \.self){
-                            Text(($0), format: .percent)
+
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
                 }
-                
+
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            
+
             .navigationTitle("WeSplit")
             .toolbar {
                 if amountIsFocused {
@@ -66,11 +60,9 @@ struct ContentView: View {
         }
     }
 }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-            
-        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
