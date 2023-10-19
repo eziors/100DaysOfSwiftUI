2013d0eaa45acf976f24f90b8ebda3d7bd30dcea
eziors <marcos.barbosa@mail.uft.edu.br>
2023-10-19 16:48:24 -0300

    Adding guess the flag

---
+++ Projetct Two - Guess the Flag/Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Marcos Barbosa on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
