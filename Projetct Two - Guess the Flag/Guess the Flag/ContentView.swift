882d3f9aff042a9651da69050c1f59303c0ab0f8
eziors <marcos.barbosa@mail.uft.edu.br>
2023-10-24 10:17:49 -0300

    Guess the flag 20% done

--- Projetct Two - Guess the Flag/Guess the Flag/ContentView.swift
+++ Projetct Two - Guess the Flag/Guess the Flag/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("The flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        print("I was tapped")
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
