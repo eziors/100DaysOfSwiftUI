f58dc2604c9bb2ed243d5e78070b478fccf50466
eziors <marcos.barbosa@mail.uft.edu.br>
2023-11-02 23:21:39 -0300

    BetterRest 20% complete

---
+++ BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marcos Barbosa on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
