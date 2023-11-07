56d9e703b3ce419160924f3e7f7a66e11007c73d
eziors <marcos.barbosa@mail.uft.edu.br>
2023-11-06 21:36:08 -0300

    WordScramble20%Done

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Marcos Barbosa on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
+    var body: some View {
+        
+        NavigationStack {
+            
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0 )
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
