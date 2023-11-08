9c671a7112b7550fce889134cd9ac92f06496122
eziors <marcos.barbosa@mail.uft.edu.br>
2023-11-07 21:39:18 -0300

    Adding words to the project

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -33,6 +33,7 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
     
@@ -44,6 +45,23 @@ struct ContentView: View {
         withAnimation {
             usedWords.insert(answer, at: 0 )
         }
+    }
+    
+    func startGame() {
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
