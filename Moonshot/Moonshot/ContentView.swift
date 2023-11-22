a6e768245b487bc4543c3b839a3b505be6946e52
eziors <marcos.barbosa@mail.uft.edu.br>
2023-11-22 00:22:38 -0300

    Moonshot adding JSON

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text(String(astronauts.count))
     }
 }
 
