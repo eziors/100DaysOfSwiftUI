b1358c69555bbac642cbab81722ab7922e051fb0
eziors <marcos.barbosa@mail.uft.edu.br>
2023-11-28 10:45:14 -0300

    Doing moonshotApp

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -10,19 +10,23 @@ import Foundation
 extension Bundle {
     func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
+            fatalError("Could not locate \(file) !!")
         }
-
+        
         guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
+            fatalError("Could not convert \(file) into data")
         }
-
+        
         let decoder = JSONDecoder()
-
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
         guard let loaded = try? decoder.decode(T.self, from: data) else {
-            fatalError("Failed to decode \(file) from bundle.")
+            fatalError("Failed to decode \(file) from Bundle.")
         }
-
         return loaded
     }
+    
 }
--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -10,13 +10,25 @@ import Foundation
 
 
 struct Mission: Codable, Identifiable {
-    struct CreRole: Codable {
+    struct CrewRole: Codable {
         let name: String
         let role: String
     }
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
     
 }
