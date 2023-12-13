2356114eb9230b93ea769fe88ad5cb39bf97a77f
eziors <marcos.barbosa@mail.uft.edu.br>
2023-12-13 19:57:15 -0300

    Adding CupcakeCorner Project

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Marcos Barbosa on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
