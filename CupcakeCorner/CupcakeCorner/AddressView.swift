e2f24e7d7cff302d00fd91ff401500bf10bd8ee7
eziors <marcos.barbosa@mail.uft.edu.br>
2023-12-16 02:42:57 -0300

    Cupcake Corner

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,21 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Marcos Barbosa on 16/12/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
+
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Marcos Barbosa on 16/12/23.
+//
+
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
+
+
