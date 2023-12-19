46ad2b1542048f13f91dec3c2076cc89112c59ab
eziors <marcos.barbosa@mail.uft.edu.br>
2023-12-19 00:09:37 -0300

    Cupcake Corner App Complete

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
     
+    @State private var confirmationMessage = "" 
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -36,11 +39,32 @@ struct CheckoutView: View {
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
     }
     func placeOrder() async {
         guard let encoded = try? JSONEncoder().encode(order) else {
             print("Failed to encode order")
             return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+            
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
         }
         
     }
