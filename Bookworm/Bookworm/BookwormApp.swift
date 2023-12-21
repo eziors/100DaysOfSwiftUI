ef6bcc827aa0054921e8232d40c5956bc007620c
eziors <marcos.barbosa@mail.uft.edu.br>
2023-12-21 03:11:47 -0300

    Bookworm App

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Marcos Barbosa on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
