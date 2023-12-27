e4a691829e172734cb1a606d5a8ee24d27827b41
eziors <marcos.barbosa@mail.uft.edu.br>
2023-12-27 16:58:04 -0300

    Creating instafilter application

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Marcos Barbosa on 27/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Marcos Barbosa on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
