acf39510cac3bc7bf1b755ecf0ddd777ca27843e
eziors <marcos.barbosa@mail.uft.edu.br>
2024-01-12 21:23:33 -0300

    adding BucketList app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Marcos Barbosa on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
