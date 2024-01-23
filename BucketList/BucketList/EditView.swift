373b511355673f48661caf02d4ad079af9416f81
eziors <marcos.barbosa@mail.uft.edu.br>
2024-01-23 11:52:52 -0300

    Adding description to BucketList App

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -36,7 +36,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                     case .loading:
@@ -83,7 +83,7 @@ struct EditView: View {
             
             let items = try JSONDecoder().decode(Result.self, from: data)
             
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
