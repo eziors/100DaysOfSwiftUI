4062b6978d9f973fc1a99384937771e28fdc94d7
eziors <marcos.barbosa@mail.uft.edu.br>
2024-01-25 02:50:43 -0300

    Adding MVVM to BucketListApp

---
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Marcos Barbosa on 24/01/24.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        private(set) var locations: [Location]
+        var selectedLocation: Location?
+        
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: point.latitude, longitude: point.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard let selectedLocation else { return }
+            
+            if let index = locations.firstIndex(of: selectedLocation) {
+                locations[index] = location
+                save()
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+    }
+}
--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,31 +8,10 @@
 import SwiftUI
 import MapKit
 
-// I Added the struct here cause i was facing a creppy error 
-struct Location: Codable, Equatable, Identifiable {
-    var id: UUID
-    var name: String
-    var description: String
-    var latitude: Double
-    var longitude: Double
-    
-    var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-    }
-    
-    #if DEBUG
-    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
-    
-    #endif
-    
-    static func ==(lhs: Location, rhs: Location) -> Bool {
-        lhs.id == rhs.id
-    }
-}
+
 
 struct ContentView: View {
-    @State private var locations = [Location]()
-    @State private var selectedLocation: Location?
+     @State private var viewModel = ViewModel()
     
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
@@ -43,7 +22,7 @@ struct ContentView: View {
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
-                ForEach(locations) { location in
+                ForEach(viewModel.locations) { location in
                     Annotation(location.name, coordinate: location.coordinate) {
                         Image(systemName: "star.circle")
                             .resizable()
@@ -52,22 +31,19 @@ struct ContentView: View {
                             .background(.white)
                             .clipShape(.circle)
                             .onLongPressGesture {
-                                selectedLocation = location
+                                viewModel.selectedLocation = location
                             }
                     }
                 }
             }
                 .onTapGesture { position in
                     if let coordinate = proxy.convert(position, from: .local) {
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                        locations.append(newLocation)
+                        viewModel.addLocation(at: coordinate)
                     }
                 }
-                .sheet(item: $selectedLocation) { place in
+                .sheet(item: $viewModel.selectedLocation) { place in
                     EditView(location: place) { newLocation in
-                        if let index = locations.firstIndex(of: place) {
-                            locations[index] = newLocation
-                        }
+                        viewModel.update(location: newLocation)
                     }
                 }
         }
